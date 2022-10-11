59d65e8823c35daedaf94a83e259d31684ac2373
Angel <soporte@libus.com>
2022-10-11 09:16:38 -0300

    Se modifican valores de api

--- site/pages/api/products.ts
+++ site/pages/api/products.ts
@@ -3,7 +3,7 @@ import data from '../../../packages/local/src/data.json'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method == 'GET') {
-    return res.status(200).json(data)
+    return res.status(200).json(data.products)
   }
   return res.status(400).json({ error: 'El metodo no existe' })
 }
